097f4188949f0a3f40007750ae8cac27d8b52c18
Nivaash <nidishnivash@gmail.com>
2023-11-04 23:14:08 +0530

    done

--- GameModePage/gamemode.js
+++ GameModePage/gamemode.js
@@ -1,5 +1,24 @@
-let play_button = document.getElementById('GameMode-start');
+let selectedOption = null;
 
-play_button.addEventListener("click",() =>{
-    window.open("../GamePage/game.html","_self")
-})
+function highlightOption(option) {
+    if (selectedOption) {
+        selectedOption.classList.remove("highlighted");
+    }
+
+    option.classList.add("highlighted");
+    selectedOption = option;
+}
+
+function redirectPage() {
+    if (selectedOption) {
+        const optionText = selectedOption.textContent;
+        switch (optionText) {
+            case "1. IronMan vs Thanos":
+                window.location.href = "../GamePage/PlayerVsPlayer/gamePLAYERvsPLAYER.html";
+                break;
+            case "2. IronMan vs Computer":
+                window.location.href = "../GamePage/playerVScomputer/gamePLAYERvsComputer.html";
+                break;
+        }
+    }
+}
